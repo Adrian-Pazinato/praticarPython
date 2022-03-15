dea577bdc7c16800cb125803b483e31780f6eb4d
Adrian Pazinato <adriandavida@outlook.com>
2022-03-15 17:02:56 -0300

    1.0

---
+++ 111.py
@@ -0,0 +1,14 @@
+import random
+
+x = 1
+d = 9123919231888
+u = 1.3
+h = 123j
+a = 34
+
+print(type(x))
+print(type(d))
+print(type(u))
+print(type(h))
+print(float(a))
+print(random.randrange(100, 1000))
