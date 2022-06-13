be00965dc789793c109690099ccaec803b7c8ab8
Adrian Pazinato <adriandavida@outlook.com>
2022-06-13 16:20:51 -0300

    5512

--- 222.py
+++ 222.py
@@ -1,4 +1,10 @@
-a = input("Enter a number: ")
-b = input("Enter another number: ")
+a = 6
+b = 5
+c = 7
 
-fds
+if a > b and a > c:
+    print("a is greater than all")
+elif b > a and b > c:
+    print("b is greater than all")
+elif c > a and c > b:
+    print("c is greater than all")
